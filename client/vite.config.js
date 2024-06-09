3686a65fa7aed69cee9c66b7ab292a5ccd423937
Putro Dwi <putrodwimly@gmail.com>
2024-06-09 20:03:13 +0700

    fix: failed to build react component

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    mainFields: [],
+    mainFields: ["browser"],
   },
-})
+});
